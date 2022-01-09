9415abda03d1c9067d169f308bfc8f38b4830efc
Patrick Hagège <hagege.patrick@gmail.com>
2022-01-09 13:15:21 +0100

    iteration 5 (deletion) before the bonus styling iteration

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+const { model, Schema } = require("mongoose");
+
+const droneSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	propellers: Number,
+    maxSpeed: Number
+});
+
+const Drone = model("Drone", droneSchema);
+
+module.exports = Drone;
